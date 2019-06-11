90242387c64ee5c07a81f0cf524c2ec6e47cd4ec
Aaron T <troskaaaron@gmail.com>
2019-06-11 18:24:09 -0500

    React app created and basic directory system set up

---
+++ instagram-app/src/Components/CommentSection/CommentSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CommentSection.css'
+
+class CommentSection extends React.component {
+  constuctor(props){
+    super(props)
+  };
+
+  render(){
+    return (
+
+    );
+  };
+};
+
+export default CommentSection;
---
+++ instagram-app/src/Components/PostContainer/PostContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './PostContainer.css'
+
+class PostContainer extends React.component {
+  constuctor(props){
+    super(props)
+  };
+
+  render(){
+    return (
+
+    );
+  };
+};
+
+export default PostContainer;
