157daef56671e2e41162578d1c795dd6230f632b
Aaron T <troskaaaron@gmail.com>
2019-06-12 20:26:56 -0500

    working on adding new comments. Still not done.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -36,24 +36,4 @@ class App extends React.Component {
   }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           stuff.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
--- instagram-app/src/Components/CommentSection/CommentSection.js
+++ instagram-app/src/Components/CommentSection/CommentSection.js
@@ -3,16 +3,34 @@ import Comment from './Comment.js';
 import NewComment from './NewComment'
 import './CommentSection.css';
 
-const CommentSection = props => {
-  // console.log('comments:',props);
-  return (
-    <div>
-      {props.comments.map(comment => { return(
-        <Comment com={comment} key={comment.id}/>
-      )})}
-      <NewComment />
-    </div>
-  );
+// const CommentSection = props => {
+//   console.log('comments:',props);
+//   return (
+//     <div>
+//       {props.comments.map(comment => { return(
+//         <Comment com={comment} key={comment.id}/>
+//       )})}
+//       <NewComment />
+//     </div>
+//   );
+// }
+
+class CommentSection extends React.Component {
+  constructor(props){
+    super(props);
+    this.state=props.comments;
+  }
+
+  render(props){
+    return(
+      <div>
+        {props.comments.map(comment => { return(
+          <Comment com={comment} key={comment.id}/>
+        )})}
+        <NewComment />
+      </div>
+    );
+  }
 }
 
 export default CommentSection;
